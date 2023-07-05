fa414f481b208374c3ca54e9d57932b2a42a2be8
Trishan Wagle <trishanwagle@pop-os.localdomain>
2023-07-05 16:38:46 +0545

    fix: resolved analytics bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+
+import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
+  integrations: [tailwind(), react()],
+  output: "server",
   adapter: vercel({
-    analytics: true,
+    analytics: true
   }),
 });
